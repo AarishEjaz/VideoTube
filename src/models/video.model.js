867eac71f279d603db0d3c798343ecc469ec6b03
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2024-12-19 00:47:39 +0530

    added some plugins

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,6 @@
+import { timeStamp } from "console";
 import mongoose from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema({
   videoFile: {
@@ -21,6 +23,21 @@ const videoSchema = new Schema({
     type: Number,
     required: true,
   },
-});
+  views:{
+    type:Number,
+    default:0
+  },
+  isPublished:{
+    type:Boolean,
+    default:true
+  },
+  owner:{
+    type:Schema.Types.ObjectId,
+    ref:"User"
+  }
+},
+{timeStamp:true});
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("Video", videoSchema)
