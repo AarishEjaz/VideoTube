310cea760ffa18da7d137345d6b5a75cbaf320e7
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2024-12-19 23:42:38 +0530

    added token generation functionality

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt "
+import dotenv from "dotenv"
+dotenv.config()
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -48,4 +50,33 @@ const userSchema = new mongoose.Schema({
   }
 },{timestamps:true});
 
+userSchma.pre("save", async function(next){
+  if(!this.isModified("password")) return next() //if negative checking is not don then you can write the encryption code in side the "if" block
+  this.password = bcrypt.hash(this.password,10) // might need to add await
+  next()
+
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+ return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign({
+    _id:this._id,
+    email:this.email,
+    username: this.username,
+    fullname: this.fullname,
+  },process.env.ACCESS_TOKEN_SECRET,{expiresIn:process.env.ACCESS_TOKEN_EXPIRY})
+}
+
+userSchema.methods.refreshAccessToken = function () {
+   return jwt.sign(
+     {
+       _id: this._id,
+     },
+     process.env.REFRESH_TOKEN_SECRET,
+     { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+   );
+}
 export const User = mongoose.model("User", userSchema)
