84b46864200308577c6d2c4ca7dfc2e6a8acf2f7
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2024-12-20 23:56:41 +0530

    added cloudinary util

---
+++ utils/cloudinary.js
@@ -0,0 +1,7 @@
+import { v2 as cloudinary } from "cloudinary";
+
+    cloudinary.config({
+      cloud_name: "dzrfreuur",
+      api_key: "334763819733622",
+      api_secret: "<your_api_secret>", // Click 'View API Keys' above to copy your API secret
+    });
