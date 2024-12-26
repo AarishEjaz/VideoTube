961ce363688d09f87fc25ea15fc00bf63af5f96a
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2024-12-26 23:00:17 +0530

    resolved error

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-import express from "express"
+
 import connectDB from './db/index.js'
 import dotenv from "dotenv"
-
+import { app } from "./app.js";
 dotenv.config({
     path:'./env'
 })
-const app = express()
+
 
 connectDB()
 .then(()=>{
