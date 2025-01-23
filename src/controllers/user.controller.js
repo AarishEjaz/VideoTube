0687dbb8784990993b1497b6a7449b779dc1225d
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-01-23 23:41:40 +0530

    added channel profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -158,7 +158,7 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
     try{
         const decodedToken = jwt.verify(
           incomingRefreshToken,
-          process.env.REFRESH_TOKEN_SECRE
+          process.env.REFRESH_TOKEN_SECRET
         );
 
         const user = await User.findById(decodedToken?._id);
@@ -282,5 +282,67 @@ const updateCoverImage = asyncHandler(async (req, res) => {
     new ApiResponse(200,"updated cover image")
   )
 });
+
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+    const {username} = req.params
+
+    if(!username?.trim()){
+        throw new ApiError(400,"Username is missing ")
+    }
+
+    const channel = User.aggregate([
+      {
+        $match: {
+          username: username?.toLowerCase(),
+        },
+      },
+      {
+        $lookup: {
+          from: "subscriptions",
+          localField: "_id",
+          foreignField: "channel",
+          as: "subscribers"
+        },
+      },
+      {
+         $lookup:{
+             from:"subscriptions",
+             localField:"_id",
+             foreignField:"subscriber",
+             as: "subscribedTo"
+         }
+        },
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers"
+                },
+                channelsSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName:1,
+                username:1,
+                subscribersCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1
+            }
+        }
+
+    ]);
+})
+
 
 export {registerUser,loginUser,logOutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccoutnDetails,updateUserAvatar,updateCoverImage}
